36a18ee115162b77e965e8539980b0b5f0990339
MASUM BILLAH <masumb2k2@gmail.com>
2026-05-12 12:39:56 +0600

    Create 02_datatypes.js

---
+++ JS/01_DataTypes/02_datatypes.js
@@ -0,0 +1,32 @@
+//Datatypes
+/*
+ String
+ Number
+ Boolean
+ BigInt
+
+ Undefined --> I dont have defination yet, but maybe or may not be fullfill later
+ Null --> Empty not 0
+
+ Object
+
+ Symbol
+
+*/
+
+// var score = 102 - old way
+
+let score = 102;
+let name = 'Masum'
+let isloggedin = true
+
+
+//object
+let teatype = ["Lemon",'orange','green','tulsi']
+let user = {firstnaem: 'Masum',lastname:'Billah'}
+
+
+let getScore = score; //borrow the value from other variable
+
+console.log(getScore);
+
