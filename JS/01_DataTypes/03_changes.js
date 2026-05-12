49ca9876600c00f5a613da1c59ff70d44fa56183
MASUM BILLAH <masumb2k2@gmail.com>
2026-05-12 12:48:52 +0600

    Create 03_changes.js

---
+++ JS/01_DataTypes/03_changes.js
@@ -0,0 +1,8 @@
+let gameName = 'spiderman';
+gameName = 'batman';
+console.log(gameName);
+
+const userName = 'masumb2k2' //can't change const variable
+// userName = 'billah'; 
+console.log(userName);
+
